894487a1f21097719a877a2f426b87f41e4c44d2
Gecko05 <jacacent@gmail.com>
2020-10-02 17:41:56 -0500

    first commit

---
+++ RailShooter/Level.gd
@@ -0,0 +1,31 @@
+extends Spatial
+export (PackedScene) var TerrainBlock
+# Declare member variables here. Examples:
+# var a = 2
+# var b = "text"
+var last_spawn_point = 0
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	var camera_pos = $Ship.transform.origin
+	camera_pos.z -= 10
+	camera_pos.y += 2
+	$Camera.transform.origin = camera_pos
+	var int_cam = int(camera_pos.z)
+	if int(camera_pos.z) % 10 == 0 and int_cam != last_spawn_point:
+		print("Spawn new square")
+		last_spawn_point = int_cam
+		var new_terrain_chunk = TerrainBlock.instance()
+		var x = Vector3(5, 0, 0)
+		var y = Vector3(0, 0.2, 0)
+		var z = Vector3(0, 0, 5)
+		new_terrain_chunk.transform.basis.x = x
+		new_terrain_chunk.transform.basis.y = y
+		new_terrain_chunk.transform.basis.z = z
+		add_child(new_terrain_chunk)
+		new_terrain_chunk.transform.origin.z = int_cam + 50
