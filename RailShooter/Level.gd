e660fe916d0dea0efc54d5ed6480ef68b5773fb4
Gecko05 <jacacent@gmail.com>
2020-10-02 22:26:24 -0500

    Added cam movement, rail restrictions and ship rotation

--- RailShooter/Level.gd
+++ RailShooter/Level.gd
@@ -11,7 +11,7 @@ func _process(delta):
 	var camera_pos = $Ship/Ship.transform.origin
 	camera_pos.z -= 10
 	camera_pos.y += 2
-	$Camera.transform.origin = camera_pos
+	$Camera.transform.origin.z = camera_pos.z
 	var int_cam = int(camera_pos.z)
 	if int(camera_pos.z) % 10 == 0 and int_cam != last_spawn_point:
 		print("Spawn new square")
@@ -25,3 +25,13 @@ func _process(delta):
 		new_terrain_chunk.transform.basis.z = z
 		add_child(new_terrain_chunk)
 		new_terrain_chunk.transform.origin.z = int_cam + 50
+	var cam_diff_x = camera_pos.x - $Camera.transform.origin.x
+	var cam_diff_y = camera_pos.y - $Camera.transform.origin.y
+	if cam_diff_x > 0.5 and $Camera.transform.origin.x < 0.5:
+		$Camera.transform.origin.x += 0.05
+	elif cam_diff_x < -0.5 and $Camera.transform.origin.x > -0.5:
+		$Camera.transform.origin.x -= 0.05
+	if cam_diff_y > 0.5 and $Camera.transform.origin.y > -0.5:
+		$Camera.transform.origin.y += 0.05
+	elif cam_diff_y < -0.5 and $Camera.transform.origin.y > -0.5:
+		$Camera.transform.origin.y -= 0.05
--- RailShooter/Player.gd
+++ RailShooter/Player.gd
@@ -2,24 +2,61 @@ extends Spatial
 
 var velocity = Vector3(0, 0, 0)
 var speed = 15
+var rot_x = 0
 var rot_y = 0
 var total_rot = 0
-const AIM_SPEED = PI/160
+const AIM_SPEED = PI/96
+const RET_AIM_SPEED = AIM_SPEED / 2
+const RAIL_X = 6
+const RAIL_Y = 10
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	velocity.z = speed
 	pass # Replace with function body.
 
-func _physics_process(delta):
+func process_y_rot():
 	if Input.is_action_pressed("move_right"):
+		if rot_y > -PI/6:
+			rot_y -= AIM_SPEED
+	elif Input.is_action_pressed("move_left"):
 		if rot_y < PI/6:
 			rot_y += AIM_SPEED
 	elif rot_y > 0:
-		rot_y -= AIM_SPEED
+		rot_y -= RET_AIM_SPEED
+	elif rot_y < 0:
+		rot_y += RET_AIM_SPEED
+
+func process_x_rot():
+	if Input.is_action_pressed("move_down"):
+		if rot_x > -PI/6:
+			rot_x -= AIM_SPEED
+	elif Input.is_action_pressed("move_up"):
+		if rot_x < PI/6:
+			rot_x += AIM_SPEED
+	elif rot_x > 0:
+		rot_x -= RET_AIM_SPEED
+	elif rot_x < 0:
+		rot_x += RET_AIM_SPEED
+
+func _physics_process(delta):
+	process_x_rot()
+	process_y_rot()
 	$Ship.transform.basis = Basis()
-	print(rot_y)
 	$Ship.rotate_object_local(Vector3(0, 1, 0), rot_y)
-	total_rot += rot_y
+	$Ship.rotate_object_local(Vector3(1, 0, 0), rot_x)
+	velocity.z = speed * cos(rot_x) * cos(rot_y)
+	velocity.x = speed * sin(rot_y)
+	velocity.y = -speed * sin(rot_x)
+	if $Ship.transform.origin.x < -RAIL_X and velocity.x < 0:
+		velocity.x = 0
+	elif $Ship.transform.origin.x > RAIL_X and velocity.x > 0:
+		velocity.x = 0
+	if $Ship.transform.origin.y < 0 and velocity.y < 0:
+		velocity.y = 0
+	elif $Ship.transform.origin.y > RAIL_Y and velocity.y > 0:
+		velocity.y = 0
+	print($Ship.transform.origin)
+	#velocity.y = -speed * sin(rot_x)
 	$Ship.move_and_slide(velocity)
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 #func _process(delta):
